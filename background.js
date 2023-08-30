1a0dccc0ac9ad4fcb64fe73148394857eca9a4a5
pruna00 <pruna@mailinator.com>
2023-08-30 13:06:16 +0000

    Downgrade to manifest v2

--- background.js
+++ background.js
@@ -1,4 +1,5 @@
-browser.action.onClicked.addListener(async () => {
+browser.browserAction.onClicked.addListener(async () => { //manifest v2
+//browser.action.onClicked.addListener(async () => { //manifest v3
     let tabs = await browser.tabs.query({ currentWindow: true });
     tabs.sort((a, b) => new Date(b.lastAccessed) - new Date(a.lastAccessed));
     browser.tabs.update(tabs[1].id, {active: true});
